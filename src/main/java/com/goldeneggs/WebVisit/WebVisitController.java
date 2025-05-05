5449d2afcc27ce456fcc2f728d33176f2b8d0f21
Esteban Pineda <estebanpinedaardila2210@gmail.com>
2025-05-04 19:19:40 -0500

    Creation of WebVisit, to keep track of visits to the website

---
+++ src/main/java/com/goldeneggs/WebVisit/WebVisitController.java
@@ -0,0 +1,28 @@
+package com.goldeneggs.WebVisit;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/visit")
+public class WebVisitController {
+
+    private final WebVisitService service;
+
+    public WebVisitController(WebVisitService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public void registerVisit(HttpServletRequest request) {
+        service.saveVisit(request);
+    }
+
+    @GetMapping("/count")
+    public long getVisitCount() {
+        return service.getVisitCount();
+    }
+}
